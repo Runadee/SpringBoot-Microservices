e91ddc9edc8dee9aab4642387cdcf10ca3c4b49d
Edanur <edanurgurgen94@gmail.com>
2024-05-09 11:42:37 +0300

    Integrate WebFlux dependencies into pom.xml and define WebClient as a Bean. Refactor get method in ServiceImpl to utilize WebClient.
    Updated project configuration in pom.xml to include WebFlux dependencies and defined WebClient as a Bean for asynchronous communication. Refactored the get method in ServiceImpl to leverage WebClient for improved performance and responsiveness

--- employee-service/src/main/java/com/edanur/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/edanur/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -17,7 +18,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
-    private RestTemplate restTemplate;
+   // private RestTemplate restTemplate;
+
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -48,11 +51,17 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         Employee employee = employeeRepository.findById(employeeId).get();
 
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"
+      /*  ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"
                                                         + employee.getDepartmentCode(),
                                                          DepartmentDto.class);
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+        DepartmentDto departmentDto = responseEntity.getBody();  */
+
+        webClient.get()
+                .uri("http://localhost:8080/api/departments/"+ employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
  
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
