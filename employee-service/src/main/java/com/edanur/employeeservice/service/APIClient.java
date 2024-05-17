69d26dc67ecfd37a4d86b62a1eef32d10259b6cf
Edanur <edanurgurgen94@gmail.com>
2024-05-17 10:47:43 +0300

    Updated project structure and configuration files
    - Modified .idea/compiler.xml, .idea/encodings.xml, and .idea/misc.xml
    - Added .gitignore file for api-gateway module
    - Added Maven wrapper files for api-gateway module
    - Added initial files for ApiGateway module:
      - ApiGatewayApplication.java
      - application.properties
      - ApiGatewayApplicationTests.java
      - pom.xml
    - Updated pom.xml and application.properties for department-service module
    - Updated APIClient.java and application.properties for employee-service module
    - Updated ServiceRegistryApplication.java and application.properties for service-registry module

--- employee-service/src/main/java/com/edanur/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/edanur/employeeservice/service/APIClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     // Build Get Department REST API
