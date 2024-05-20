cb9ab919ea6c39e2d7ba5af8c87ce4623a267861
Edanur <edanurgurgen94@gmail.com>
2024-05-20 10:25:41 +0300

    Add initial setup for Config Server
    - Added .gitignore
    - Added Maven wrapper files
    - Added pom.xml
    - Added ConfigServerApplication.java
    - Added application.properties
    - Added ConfigServerApplicationTests.java
    
    Initial setup for centralized configuration management.

---
+++ config-server/src/test/java/com/edanur/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edanur.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
